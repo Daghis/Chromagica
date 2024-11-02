213487b99c6efda2b260914967180a5fe114c9cf
Marc Chametzky <marc@bluevine.net>
2024-11-02 17:29:36 -0400

    Adding filament stack determination for a particular RGBColor

---
+++ src/main/java/net/bluevine/chromagica/FilamentStacker.java
@@ -0,0 +1,145 @@
+package net.bluevine.chromagica;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.Collections.emptyList;
+import static java.util.Comparator.comparingDouble;
+import static net.bluevine.chromagica.ColorUtil.getColorForSequence;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Iterables;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Stream;
+import lombok.Builder;
+import lombok.Value;
+import net.bluevine.chromagica.model.FilamentData;
+import net.bluevine.chromagica.model.RGBColor;
+
+public class FilamentStacker {
+  private final Map<String, FilamentData> filamentData;
+  private final String baseFilament;
+  private final int maxLayers;
+
+  @Value
+  @Builder
+  public static class FilamentStack {
+    List<String> filamentSequence;
+    RGBColor color;
+    double differenceFromTarget;
+  }
+
+  public FilamentStacker(
+      Map<String, FilamentData> filamentData, String baseFilament, int maxLayers) {
+    this.filamentData =
+        ImmutableMap.copyOf(checkNotNull(filamentData, "filamentData cannot be null"));
+    this.baseFilament = checkNotNull(baseFilament, "baseFilament cannot be null");
+    checkArgument(maxLayers > 0, "maxLayers must be positive");
+    this.maxLayers = maxLayers;
+  }
+
+  private static final int BEAM_WIDTH = 50;
+
+  @Value
+  @Builder
+  private static class BeamEntry {
+    RGBColor color;
+    List<String> filamentSequence;
+    double differenceFromTarget;
+  }
+
+  public FilamentStack optimizeColorSequence(RGBColor targetColor) {
+    return optimizeColorSequence(targetColor, emptyList());
+  }
+
+  private TreeSet<BeamEntry> initializeBeam(RGBColor targetColor, List<String> topColors) {
+    TreeSet<BeamEntry> beam = new TreeSet<>(comparingDouble(BeamEntry::getDifferenceFromTarget));
+    // Start off the beam with 1 layer of each filament.
+    for (Entry<String, FilamentData> filamentDataEntry : filamentData.entrySet()) {
+      String filament = filamentDataEntry.getKey();
+      if (!topColors.isEmpty() && !Iterables.getLast(topColors).equals(filament)) {
+        continue;
+      }
+
+      List<String> sequence = ImmutableList.of(filamentDataEntry.getKey());
+      RGBColor color = getCachedColorForSequence(sequence);
+      beam.add(
+          BeamEntry.builder()
+              .color(color)
+              .filamentSequence(sequence)
+              .differenceFromTarget(color.computeLabDistance(targetColor))
+              .build());
+    }
+
+    return beam;
+  }
+
+  public FilamentStack optimizeColorSequence(RGBColor targetColor, List<String> topColors) {
+    TreeSet<BeamEntry> beam = initializeBeam(targetColor, topColors);
+
+    int lastBeamSize = 0;
+    while (lastBeamSize < beam.size()) {
+      lastBeamSize = beam.size();
+
+      // Find the best entries from the beam to work with.
+      List<List<String>> sequenceCandidates =
+          beam.stream()
+              .map(BeamEntry::getFilamentSequence)
+              // Ensure that candidates have room for one more filament in the sequence.
+              .filter(sequence -> sequence.size() < maxLayers)
+              .limit(BEAM_WIDTH)
+              .collect(toImmutableList());
+
+      for (List<String> sequence : sequenceCandidates) {
+        for (String filament : filamentData.keySet()) {
+          List<String> newSequence =
+              Stream.concat(Stream.of(filament), sequence.stream()).collect(toImmutableList());
+
+          RGBColor newColor = getCachedColorForSequence(newSequence);
+          double difference = newColor.computeLabDistance(targetColor);
+          beam.add(
+              BeamEntry.builder()
+                  .color(newColor)
+                  .filamentSequence(newSequence)
+                  .differenceFromTarget(difference)
+                  .build());
+        }
+      }
+    }
+
+    BeamEntry best = checkNotNull(beam.first());
+    List<String> bestSequence = best.getFilamentSequence();
+    // Ensure that the result is padded to maxLayers.
+    bestSequence =
+        Stream.concat(
+                Collections.nCopies(maxLayers - bestSequence.size(), baseFilament).stream(),
+                bestSequence.stream())
+            .collect(toImmutableList());
+
+    return FilamentStack.builder()
+        .filamentSequence(ImmutableList.copyOf(bestSequence))
+        .color(best.getColor())
+        .differenceFromTarget(best.getDifferenceFromTarget())
+        .build();
+  }
+
+  private final Cache<List<String>, RGBColor> colorCache =
+      CacheBuilder.newBuilder().maximumSize(1000).build();
+
+  private RGBColor getCachedColorForSequence(List<String> sequence) {
+    try {
+      return colorCache.get(
+          sequence, () -> getColorForSequence(sequence, filamentData, baseFilament));
+    } catch (ExecutionException e) {
+      throw new RuntimeException(e.getCause());
+    }
+  }
+}
---
+++ src/test/java/net/bluevine/chromagica/FilamentStackerTest.java
@@ -0,0 +1,133 @@
+package net.bluevine.chromagica;
+
+import static java.util.Collections.emptyMap;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import net.bluevine.chromagica.FilamentStacker.FilamentStack;
+import net.bluevine.chromagica.model.FilamentData;
+import net.bluevine.chromagica.model.RGBCoefficients;
+import net.bluevine.chromagica.model.RGBColor;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+class FilamentStackerTest {
+  private static final RGBColor BLUE_COLOR = new RGBColor(7, 36, 153);
+  private static final RGBCoefficients BLUE_COEFFICIENTS =
+      new RGBCoefficients(
+          new double[] {4.480981432791474, 0.4443952684960717, -6.000013383278259E-4},
+          new double[] {24.75142700938747, 0.35721514086022566, 4.944319721599738E-4},
+          new double[] {20.889749735873615, 0.9464429696439061, -5.933063972022456E-4});
+  private static final FilamentData BLUE_FILAMENT_DATA =
+      new FilamentData(BLUE_COLOR, BLUE_COEFFICIENTS, emptyMap());
+
+  private static final RGBColor CYAN_COLOR = new RGBColor(18, 94, 195);
+  private static final RGBCoefficients CYAN_COEFFICIENTS =
+      new RGBCoefficients(
+          new double[] {10.104933275172417, 0.5606841631632997, -9.996605964241558E-4},
+          new double[] {45.47346601829285, 0.5641957131366364, -1.5870963339774738E-4},
+          new double[] {50.81003367840157, 0.8225173880459974, -4.452842482286347E-4});
+  private static final FilamentData CYAN_FILAMENT_DATA =
+      new FilamentData(CYAN_COLOR, CYAN_COEFFICIENTS, emptyMap());
+
+  private static final RGBColor WHITE_COLOR = new RGBColor(254, 254, 254);
+  private static final RGBCoefficients WHITE_COEFFICIENTS =
+      new RGBCoefficients(
+          new double[] {64.88128463300333, 0.7721479787432053, -1.4653425210567843E-4},
+          new double[] {62.17763968106627, 0.8971743277462471, -6.616349721127205E-4},
+          new double[] {60.3418795814688, 0.9280928848575695, -7.244634245743158E-4});
+  private static final FilamentData WHITE_FILAMENT_DATA =
+      new FilamentData(WHITE_COLOR, WHITE_COEFFICIENTS, emptyMap());
+
+  @Test
+  void filamentStacker_nullFilamentData() {
+    assertThrows(NullPointerException.class, () -> new FilamentStacker(null, "White", 8));
+  }
+
+  @Test
+  void filamentStacker_nullBaseFilament() {
+    assertThrows(NullPointerException.class, () -> new FilamentStacker(emptyMap(), null, 8));
+  }
+
+  @Test
+  void filamentStacker_negativeLayers() {
+    assertThrows(
+        IllegalArgumentException.class, () -> new FilamentStacker(emptyMap(), "White", -1));
+  }
+
+  @Test
+  void optimizeColorSequence_simple() {
+    Map<String, FilamentData> filamentData = ImmutableMap.of("White", WHITE_FILAMENT_DATA);
+
+    FilamentStacker stacker = new FilamentStacker(filamentData, "White", 1);
+
+    FilamentStack result = stacker.optimizeColorSequence(WHITE_COLOR);
+
+    assertEquals(List.of("White"), result.getFilamentSequence());
+  }
+
+  @Test
+  void optimizeColorSequence_colors() {
+    Map<String, FilamentData> filamentData =
+        ImmutableMap.of(
+            "Blue", BLUE_FILAMENT_DATA, "Cyan", CYAN_FILAMENT_DATA, "White", WHITE_FILAMENT_DATA);
+
+    FilamentStacker stacker = new FilamentStacker(filamentData, "White", 4);
+
+    FilamentStack result = stacker.optimizeColorSequence(new RGBColor(100, 150, 225));
+
+    assertEquals(List.of("White", "Cyan", "Blue", "White"), result.getFilamentSequence());
+  }
+
+  @Test
+  void optimizeColorSequence_topColor() {
+    Map<String, FilamentData> filamentData =
+        ImmutableMap.of(
+            "Blue", BLUE_FILAMENT_DATA, "Cyan", CYAN_FILAMENT_DATA, "White", WHITE_FILAMENT_DATA);
+
+    FilamentStacker stacker = new FilamentStacker(filamentData, "White", 4);
+
+    FilamentStack result =
+        stacker.optimizeColorSequence(new RGBColor(100, 150, 225), ImmutableList.of("Cyan"));
+
+    assertEquals(List.of("White", "Blue", "White", "Cyan"), result.getFilamentSequence());
+  }
+
+  @Test
+  void optimizeColorSequence_cacheException() throws Exception {
+    Map<String, FilamentData> filamentData =
+        ImmutableMap.of(
+            "Blue", BLUE_FILAMENT_DATA, "Cyan", CYAN_FILAMENT_DATA, "White", WHITE_FILAMENT_DATA);
+    try (MockedStatic<CacheBuilder> mockedCacheBuilder = mockStatic(CacheBuilder.class)) {
+      Cache<List<String>, RGBColor> mockedCache = mock(Cache.class);
+      CacheBuilder<List<String>, RGBColor> cacheBuilder = mock(CacheBuilder.class);
+      when(cacheBuilder.build()).thenReturn(mockedCache);
+      mockedCacheBuilder.when(CacheBuilder::newBuilder).thenReturn(cacheBuilder);
+
+      when(mockedCache.get(anyList(), any()))
+          .thenThrow(new ExecutionException(new IllegalArgumentException("Mocked exception")));
+
+      // Manually test if the mock works as expected
+      FilamentStacker stacker = new FilamentStacker(filamentData, "White", 4);
+
+      RuntimeException thrown =
+          assertThrows(
+              RuntimeException.class,
+              () -> stacker.optimizeColorSequence(new RGBColor(100, 150, 225)));
+      assertInstanceOf(IllegalArgumentException.class, thrown.getCause());
+    }
+  }
+}
