0ca62823c1f4c2f7924bfeefaa8069de86e49008
Marc Chametzky <marc@bluevine.net>
2024-11-03 13:32:21 -0500

    Updates to tests for ColorUtil

--- src/test/java/net/bluevine/chromagica/ColorUtilTest.java
+++ src/test/java/net/bluevine/chromagica/ColorUtilTest.java
@@ -1,66 +1,84 @@
 package net.bluevine.chromagica;
 
+import static net.bluevine.chromagica.TestData.BLUE_COLOR;
+import static net.bluevine.chromagica.TestData.CYAN_COLOR;
+import static net.bluevine.chromagica.TestData.TEST_FILAMENT_DATA_MAP;
+import static net.bluevine.chromagica.TestData.WHITE_COLOR;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Multiset;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 import java.util.List;
 import net.bluevine.chromagica.model.RGBColor;
 import org.junit.jupiter.api.Test;
 
 class ColorUtilTest {
-  static final double FLOAT_DELTA = 0.00001;
+  static final double FLOAT_DELTA = 0.001;
 
   @Test
-  void calculateDifference() {
-    RGBColor color1 = new RGBColor(255, 0, 0);
-    RGBColor color2 = new RGBColor(255, 255, 255);
-    double expected = 11.55123;
-
-    double result = ColorUtil.calculateDifference(color1, color2);
-
-    assertEquals(expected, result, FLOAT_DELTA);
+  void getColorForSequence_basic() {
+    List<String> sequence = List.of("Blue", "White", "Cyan");
+    RGBColor expected = new RGBColor(64.49, 141.836, 217.282);
+    RGBColor result = ColorUtil.getColorForSequence(sequence, TEST_FILAMENT_DATA_MAP, "White");
+    assertAll(
+        () -> assertEquals(expected.getR(), result.getR(), FLOAT_DELTA),
+        () -> assertEquals(expected.getG(), result.getG(), FLOAT_DELTA),
+        () -> assertEquals(expected.getB(), result.getB(), FLOAT_DELTA));
   }
 
   @Test
-  void calculateDifference_zeroDifference() {
-    RGBColor color = new RGBColor(255, 180, 240);
-
-    double result = ColorUtil.calculateDifference(color, color);
-
-    assertEquals(0, result, FLOAT_DELTA);
+  void getColorForSequence_emptySequence() {
+    RGBColor result =
+        ColorUtil.getColorForSequence(ImmutableList.of(), TEST_FILAMENT_DATA_MAP, "Cyan");
+    assertEquals(CYAN_COLOR, result);
   }
 
   @Test
-  void getAverageColor() {
-    List<RGBColor> colors = List.of(new RGBColor(0, 0, 255), new RGBColor(255, 255, 0));
-    RGBColor expected = new RGBColor(128, 128, 128);
+  void getColorForSequence_reusedResults() {
+    List<String> primingSequence = List.of("Blue", "White", "Blue");
+    List<String> sequence = List.of("Blue", "White", "White");
+    RGBColor expected = new RGBColor(158.809, 202.182, 236.166);
 
-    RGBColor result = ColorUtil.getAverageColor(colors);
+    ColorUtil.getColorForSequence(primingSequence, TEST_FILAMENT_DATA_MAP, "White");
 
-    assertEquals(expected, result);
+    RGBColor result = ColorUtil.getColorForSequence(sequence, TEST_FILAMENT_DATA_MAP, "White");
+    assertAll(
+        () -> assertEquals(expected.getR(), result.getR(), FLOAT_DELTA),
+        () -> assertEquals(expected.getG(), result.getG(), FLOAT_DELTA),
+        () -> assertEquals(expected.getB(), result.getB(), FLOAT_DELTA));
   }
 
   @Test
-  void getAverageColor_null() {
+  void getDominantColor_basic() {
+    Multiset<RGBColor> input = HashMultiset.create(List.of(WHITE_COLOR, BLUE_COLOR, BLUE_COLOR));
+    RGBColor result = ColorUtil.getDominantColor(input);
+
+    assertEquals(BLUE_COLOR, result);
+  }
+
+  @Test
+  void getDominantColor_fallback() {
     RGBColor expected = new RGBColor(0, 0, 0);
 
-    RGBColor result = ColorUtil.getAverageColor(null);
-
-    assertEquals(expected, result);
+    assertEquals(expected, ColorUtil.getDominantColor(null));
   }
 
   @Test
-  void getAverageColor_empty() {
-    RGBColor expected = new RGBColor(0, 0, 0);
+  void getDominantColors_nullInput() {
+    assertTrue(ColorUtil.getDominantColors(null, 10).isEmpty());
+  }
 
-    RGBColor result = ColorUtil.getAverageColor(new ArrayList<>());
-
-    assertEquals(expected, result);
+  @Test
+  void getDominantColors_emptyInput() {
+    assertTrue(ColorUtil.getDominantColors(ImmutableList.of(), 10).isEmpty());
   }
 
   @Test
