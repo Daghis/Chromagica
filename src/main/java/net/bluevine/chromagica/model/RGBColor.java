277b1464ccd2362e6bd08c0389c090e50456f3b2
Marc Chametzky <marc@bluevine.net>
2024-11-03 07:49:25 -0500

    Update to RGBColor

--- src/main/java/net/bluevine/chromagica/model/RGBColor.java
+++ src/main/java/net/bluevine/chromagica/model/RGBColor.java
@@ -1,54 +1,99 @@
 package net.bluevine.chromagica.model;
 
+import static org.apache.commons.imaging.color.ColorConversions.convertRgbToXyz;
+import static org.apache.commons.imaging.color.ColorConversions.convertXyzToCieLab;
+
 import java.awt.Color;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Value;
+import org.apache.commons.imaging.color.ColorCieLab;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Value
 public class RGBColor implements Comparable<RGBColor> {
-  private int[] rgb = new int[3];
+  double[] rgb = new double[3];
+  ColorCieLab lab;
 
-  public RGBColor(Color color) {
-    rgb[0] = color.getRed();
-    rgb[1] = color.getGreen();
-    rgb[2] = color.getBlue();
-  }
-
-  public RGBColor(int color) {
-    rgb[0] = (color >> 16) & 0xFF;
-    rgb[1] = (color >> 8) & 0xFF;
-    rgb[2] = (color) & 0xFF;
-  }
-
-  public RGBColor(int r, int g, int b) {
+  public RGBColor(double r, double g, double b) {
     rgb[0] = r;
     rgb[1] = g;
     rgb[2] = b;
+
+    lab = convertXyzToCieLab(convertRgbToXyz(getRGB()));
   }
 
-  public int getR() {
+  public RGBColor(Color color) {
+    this(color.getRed(), color.getGreen(), color.getBlue());
+  }
+
+  public RGBColor(int color) {
+    this((color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF);
+  }
+
+  private double clamp(double value) {
+    return Math.max(0, Math.min(255, value));
+  }
+
+  public double getR() {
     return rgb[0];
   }
 
-  public int getG() {
+  public int getRInt() {
+    return Math.round((float) clamp(rgb[0]));
+  }
+
+  public double getG() {
     return rgb[1];
   }
 
-  public int getB() {
+  // Corrected the index from rgb[2] to rgb[1]
+  public int getGInt() {
+    return Math.round((float) clamp(rgb[1]));
+  }
+
+  public double getB() {
     return rgb[2];
+  }
+
+  public int getBInt() {
+    return Math.round((float) clamp(rgb[2]));
+  }
+
+  public int getRGB() {
+    // Combine the components into a single integer (0xRRGGBB)
+    return 0xff << 24 | getRInt() << 16 | getGInt() << 8 | getBInt();
+  }
+
+  public int[] getIntArray() {
+    return new int[] {getRInt(), getGInt(), getBInt()};
+  }
+
+  /**
+   * Computes the Euclidean distance between two RGBColor instances in Lab color space.
+   *
+   * @param other the other RGBColor
+   * @return the Euclidean distance between this object and other in Lab space
+   */
+  public double computeLabDistance(RGBColor other) {
+    double deltaL = lab.l - other.lab.l;
+    double deltaA = lab.a - other.lab.a;
+    double deltaB = lab.b - other.lab.b;
+
+    return Math.sqrt(deltaL * deltaL + deltaA * deltaA + deltaB * deltaB);
   }
 
   @Override
   public int compareTo(RGBColor other) {
-    int result = Integer.compare(this.getR(), other.getR());
+    int result = Double.compare(this.getR(), other.getR());
     if (result != 0) return result;
 
-    result = Integer.compare(this.getG(), other.getG());
+    result = Double.compare(this.getG(), other.getG());
     if (result != 0) return result;
 
-    return Integer.compare(this.getB(), other.getB());
+    return Double.compare(this.getB(), other.getB());
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "RGBColor(rgb=#%06x, lab={L=%.2f, a=%.2f, b=%.2f})", getRGB(), lab.l, lab.a, lab.b);
   }
 }
